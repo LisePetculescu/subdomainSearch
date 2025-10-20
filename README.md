b33c9bfb90dd79844b74517976a5c9fd3338d8fd
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2025-10-20 13:51:16 +0200

    base code for dictionary subdomain search

---
+++ README.md
@@ -0,0 +1,3 @@
+## This is a project for trying Dictionary subdomain search
+
+### Code and idea inspired by: Web Application Security 2ed - Andrew Hoffman 2024
--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello");
+import dns from "dns";
+import fs from "fs";
+import csv from "csv-parser";
+
+import dns from "dns/promises"; // Node 18+
+import fs from "fs";
+import csv from "csv-parser";
+
+const promises = [];
+
+fs.createReadStream("subdomains-10000.txt")
+  .pipe(csv())
+  .on("data", (subdomain) => {
+    promises.push(
+      dns
+        .resolve(`${subdomain}.mega-bank.com`)
+        .then((ip) => ({ subdomain, ip }))
+        .catch(() => ({ subdomain, ip: null }))
+    );
+  })
+  .on("end", async () => {
+    const results = await Promise.all(promises);
+    results.forEach((result) => {
+      if (result.ip) console.log(result);
+    });
+  });
